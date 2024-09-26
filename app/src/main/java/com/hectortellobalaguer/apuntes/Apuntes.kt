a4bfe8f131ddec5430bfa57b42c0795ce3b3e3be
hector <hilariocamachomore@gmail.com>
2024-09-26 19:02:25 +0200

    commit inicial

---
+++ app/src/main/java/com/hectortellobalaguer/apuntes/Apuntes.kt
@@ -0,0 +1,68 @@
+package com.hectortellobalaguer.apuntes
+
+fun main() {
+//    muestraDeVariables()
+//    entradaSalidaDatos()
+    controlNulos()
+//    operadores()
+
+}
+fun controlNulos(){
+    /*En este procedimiento se van a explicar los operadores de control de nulos*/
+
+    var nombre:String? = null
+
+//    println(nombre.length)
+    if(nombre != null){
+        println(nombre.length)
+    }
+}
+
+fun entradaSalidaDatos(){
+    //Escribir por consola
+    print("Hola")
+    println(" como estas")
+
+    println("Hola \n como estas")
+
+    //Leer de teclado
+    val nombre = readLine()
+    println("Hola $nombre")
+    println("Hola " + nombre)
+}
+
+fun muestraDeVariables(){
+
+    var varIntExplicito:Int = 100
+    var varIntInferido = 214875
+
+    var varLongExplicito:Long = 325684874512
+    var varLongInferido = 214875L
+
+    var varFloatExplicito:Float = 3256848745.12F
+    var varFloatInferido = 214.875F
+
+    var varDoubleExplicito:Double = 325684874512.5
+    var varDoubleInferido = 214875.546523123
+
+    var numeroFavoritoChar: Char = '1'
+    var letraFavoritaChar: Char = 'q'
+    var caracterFavorito = '@'
+
+    var numeroFavorito: String = "Mi número favorito es el 3"
+    var test = "Test. 12345!·$%&/"
+
+    var estoyTriste: Boolean = false
+    var estoyFeliz = true
+
+    println("varIntExplicito: $varIntExplicito")
+    println("varLongExplicito: $varLongExplicito")
+    println("varFloatExplicito: $varFloatExplicito")
+    println("varDoubleInferido : $varDoubleInferido")
+    println("caracterFavorito : $caracterFavorito")
+    println("numeroFavorito : $numeroFavorito")
+    println("test : $test")
+    println("estoyFeliz : $estoyFeliz")
+
+}
+
