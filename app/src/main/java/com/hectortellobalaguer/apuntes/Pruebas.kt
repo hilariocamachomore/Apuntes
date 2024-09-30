2adf22d19e89c59ed6e8ba3b4c6ea3032cf782db
hector <hilariocamachomore@gmail.com>
2024-09-30 20:41:55 +0200

    commit serio con apuntes y demás

---
+++ app/src/main/java/com/hectortellobalaguer/apuntes/Pruebas.kt
@@ -0,0 +1,22 @@
+package com.hectortellobalaguer.apuntes
+
+fun main() {
+    val numeros = mutableListOf<Double>()
+    var contador = 0
+
+    while (contador < 5) {
+        print("Introduce el número ${contador + 1}: ")
+        val input = readlnOrNull()
+
+        try {
+            val numero = input?.toDouble() ?: throw NumberFormatException()
+            numeros.add(numero)
+            contador++
+        } catch (e: NumberFormatException) {
+            println("Entrada inválida. Introduce un número válido.")
+        }
+    }
+
+    val media = numeros.average()
+    println("La media de los números es: $media")
+}
