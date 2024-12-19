743cffb10204f8657289addf91b8981fb5486cb4
hector <hilariocamachomore@gmail.com>
2024-12-19 19:22:40 +0100

    commit bucles completo casa

--- app/src/main/java/com/hectortellobalaguer/apuntes/Ejercicios.kt
+++ app/src/main/java/com/hectortellobalaguer/apuntes/Ejercicios.kt
@@ -1,7 +1,22 @@
 package com.hectortellobalaguer.apuntes
 
 fun main() {
+    println("Introduce un número entero:")
+    val numero = readLine()?.toIntOrNull() ?: 0
+    if (esPrimo(numero)) {
+        println("$numero es un número primo.")
+    } else {
+        println("$numero no es un número primo.")
+    }
+}
 
+
+fun esPrimo(numero: Int): Boolean {
+    if (numero <= 1) return false
+    for (i in 2 until numero) {
+        if (numero % i == 0) return false
+    }
+    return true
 }
 
 fun ejercicio1() {
