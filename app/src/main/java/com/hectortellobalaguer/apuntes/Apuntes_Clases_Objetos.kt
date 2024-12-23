bb298767ae3ac4dc0e8b768ebbc3cb7be97fd6ae
hector <hilariocamachomore@gmail.com>
2024-12-23 20:43:35 +0100

    commit bucles completo casa

--- app/src/main/java/com/hectortellobalaguer/apuntes/Apuntes_Clases_Objetos.kt
+++ app/src/main/java/com/hectortellobalaguer/apuntes/Apuntes_Clases_Objetos.kt
@@ -7,6 +7,7 @@ class Persona(val nombre:String, val edad:Int){
 }
 
 class PersonaDos(val nombre:String, val edad:Int){
+
     constructor(nombre:String):this(nombre, 0)
     constructor():this("Fulanito", 0)
 
@@ -15,15 +16,37 @@ class PersonaDos(val nombre:String, val edad:Int){
     }
 }
 
+class Coche(private var marca:String, var modelo:String){
+    fun muestraMarca(){
+        println("La marca del coche es $marca")
+    }
+    fun cambiaMarca(nuevaMarca:String){
+        marca = nuevaMarca
+    }
+
+}
+
 fun main(){
     val persona1 = Persona("Juan", 25)
     persona1.saludar()
+
+    //Constructores
     val pepe = PersonaDos("Pepe",45)
     pepe.saludar()
     val pepeDos = PersonaDos("Pepe")
     pepeDos.saludar()
     val pepeTres = PersonaDos()
     pepeTres.saludar()
+
+    //Encapsulamiento
+    var micoche = Coche("Toyota", "Corolla")
+    micoche.muestraMarca()
+    println(micoche.modelo)
+    micoche.cambiaMarca("Ford")
+    micoche.modelo = "Mustang"
+    micoche.muestraMarca()
+    println(micoche.modelo)
+
 
 
 
