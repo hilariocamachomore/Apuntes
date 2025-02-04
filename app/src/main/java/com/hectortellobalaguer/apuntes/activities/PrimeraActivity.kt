aafe7cf14a6d9531829f2c3b1edebc3cc62c9256
HECTOR <hilariocamachomore@gmail.com>
2025-02-04 09:50:05 +0100

    Metida activiy primera

--- app/src/main/java/com/hectortellobalaguer/apuntes/activities/PrimeraActivity.kt
+++ app/src/main/java/com/hectortellobalaguer/apuntes/activities/PrimeraActivity.kt
@@ -1,34 +1,21 @@
-package com.hectortellobalaguer.apuntes
+package com.hectortellobalaguer.apuntes.activities
 
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.hectortellobalaguer.apuntes.R
 
-class MenuActivity : AppCompatActivity() {
-    var btn1: Button?= null;
+class PrimeraActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_menu)
+        setContentView(R.layout.activity_primera)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        btn1 = findViewById(R.id.btnActivityPrimera)
-        btn1?.setOnClickListener {
-            prueba()
-        }
-
     }
-
-    private fun prueba() {
-        println("jola")
-
-    }
-
-
 }
