5276ed8b5b59c9817cdb01e8e16faf76f5546b46
HECTOR <hilariocamachomore@gmail.com>
2025-02-05 13:58:50 +0100

    Anlazado boton don activity primera

--- app/src/main/java/com/hectortellobalaguer/apuntes/MenuActivity.kt
+++ app/src/main/java/com/hectortellobalaguer/apuntes/MenuActivity.kt
@@ -1,14 +1,17 @@
 package com.hectortellobalaguer.apuntes
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.hectortellobalaguer.apuntes.activities.PrimeraActivity
 
 class MenuActivity : AppCompatActivity() {
-    var btn1: Button?= null;
+    var btnActivityPrimera: Button?= null;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,16 +21,18 @@ class MenuActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        btn1 = findViewById(R.id.btnActivityPrimera)
-        btn1?.setOnClickListener {
-            prueba()
+        btnActivityPrimera = findViewById(R.id.btnActivityPrimera)
+        btnActivityPrimera?.setOnClickListener {
+            irActividadPrimera()
         }
 
     }
 
-    private fun prueba() {
-        println("jola")
+    private fun irActividadPrimera() {
 
+        Toast.makeText(this, "prueba", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, PrimeraActivity::class.java)
+        startActivity(intent)
     }
 
 
