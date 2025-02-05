f58483b17c8a06bbb9ac90b2ffe1903abc242c5c
hector <hilariocamachomore@gmail.com>
2025-02-05 20:39:03 +0100

    Contemplados todos incluido POO. Solo faltan ejercicios de POO

--- app/src/main/java/com/hectortellobalaguer/apuntes/Apuntes_Clases_Objetos.kt
+++ app/src/main/java/com/hectortellobalaguer/apuntes/Apuntes_Clases_Objetos.kt
@@ -26,19 +26,73 @@ class Coche(private var marca:String, var modelo:String){
 
 }
 
-fun main(){
-    val persona1 = Persona("Juan", 25)
-    persona1.saludar()
+open class Animal(val nombre:String, val edad:Int){
+    open fun hacerSonido(){
+        println("El animal hace un sonido")
+    }
+}
 
-    //Constructores
+class Perro(nombre:String, edad:Int):Animal(nombre, edad){
+    override fun hacerSonido(){
+        println("El perro $nombre de $edad años de edad hace guau")
+    }
+}
+
+open class Vehiculo() {
+    open fun tipoDeVehiculo() {
+        println("Vehículo")
+    }
+}
+
+class Moto : Vehiculo() {
+    override fun tipoDeVehiculo() {
+        println("Este es un amoto")
+    }
+}
+class Barco : Vehiculo() {
+    override fun tipoDeVehiculo() {
+        println("Este es un barco")
+    }
+}
+abstract class Figura {
+    abstract fun calcularArea(): Double
+    fun mostrar(){
+        println("Es una figura geométrica")
+    }
+}
+class Cuadrado(val lado: Double) : Figura() {
+    override fun calcularArea(): Double {
+        return lado * lado
+    }
+}
+
+
+fun main(){
+
+//    InstanciaObjetos()
+//    Constructores()
+//    Encapsulamiento()
+//    Herencia()
+//    Polimorfismo()
+    Abstraccion()
+
+}
+
+fun InstanciaObjetos() {
+    val persona1 = Persona("Miguela", 25)
+    persona1.saludar()
+}
+
+fun Constructores() {
     val pepe = PersonaDos("Pepe",45)
     pepe.saludar()
     val pepeDos = PersonaDos("Pepe")
     pepeDos.saludar()
     val pepeTres = PersonaDos()
     pepeTres.saludar()
+}
 
-    //Encapsulamiento
+fun Encapsulamiento() {
     var micoche = Coche("Toyota", "Corolla")
     micoche.muestraMarca()
     println(micoche.modelo)
@@ -46,8 +100,25 @@ fun main(){
     micoche.modelo = "Mustang"
     micoche.muestraMarca()
     println(micoche.modelo)
-
-
-
-
 }
+
+fun Herencia() {
+    val animal = Animal("Fido", 3)
+    animal.hacerSonido()
+    val migoss = Perro("Migue", 5)
+    migoss.hacerSonido()
+}
+
+fun Polimorfismo() {
+    val vehiculo1: Vehiculo = Moto()
+    val vehiculo2: Vehiculo = Barco()
+    vehiculo1.tipoDeVehiculo()
+    vehiculo2.tipoDeVehiculo()
+}
+
+fun Abstraccion() {
+    val cuadrado = Cuadrado(5.0)
+    cuadrado.mostrar()
+    println("El área del cuadrado es: ${cuadrado.calcularArea()}")
+}
+
